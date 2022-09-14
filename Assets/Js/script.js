0ea246b19318990117ce2112db3a5f2346983933
Liliana Ormeno <liliana.ormenoc@gmail.com>
2022-09-14 15:47:53 -0300

    first commit

---
+++ Assets/Js/script.js
@@ -0,0 +1,8 @@
+
+//popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//tootip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
